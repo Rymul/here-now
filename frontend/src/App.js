3dc6db9c40e51b8830fdf78b853256fa8e3017ad
Jay Reddy <jayreddy@gmail.com>
2022-09-20 16:23:22 -0700

    Gets working navbar, adjusts route on App.js (changed from AuthRoute)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { Redirect, Switch } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import { Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 
 import SplashPage from './components/SplashPage/SplashPage';
@@ -23,7 +24,7 @@ function App() {
     <>
       <NavBar />
       <Switch>
-        <AuthRoute exact path="/" component={SplashPage} />
+        <Route exact path="/" component={SplashPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
         {/* <ProtectedRoute exact path="/feed" component={EventsIndex}/>
--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './NavBar.css';
 import { logout } from '../../store/session';
+import { FcMenu } from 'react-icons/fc'
+
 
 function NavBar () {
   const loggedIn = useSelector(state => !!state.session.user);
@@ -12,30 +14,42 @@ function NavBar () {
       dispatch(logout());
   }
 
-  const getLinks = () => {
-    if (loggedIn) {
-      return (
-        <div className="links-nav">
-          <Link to={'/events'}>All Events</Link>
-          <Link to={'/profile'}>Profile</Link>
-          <Link to={'/tweets/new'}>Write a Tweet</Link>
-          <button onClick={logoutUser}>Logout</button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="links-auth">
-          <Link to={'/signup'}>Signup</Link>
-          <Link to={'/login'}>Login</Link>
-        </div>
-      );
-    }
-  }
+  // const getLinks = () => {
+  //   if (loggedIn) {
+  //     return (
+  //       <div className="links-nav">
+  //         <Link to={'/events'}>All Events</Link>
+  //         <Link to={'/profile'}>Profile</Link>
+  //         <Link to={'/tweets/new'}>Write a Tweet</Link>
+  //         <button onClick={logoutUser}>Logout</button>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div className="links-auth">
+  //         <Link to={'/signup'}>Signup</Link>
+  //         <Link to={'/login'}>Login</Link>
+  //       </div>
+  //     );
+  //   }
+  // }
 
   return (
     <>
-      <h1>Chirper</h1>
-      { getLinks() }
+      {/* <h1>Chirper</h1>
+      { getLinks() } */}
+
+      <div className="nav-bar">
+        <div id="nav-bar-hamburger-subdiv" className="nav-bar-subdiv">
+          <FcMenu className="nav-bar-hamburger" />
+        </div>
+        <div id="nav-bar-subdiv-logo" className="nav-bar-subdiv">
+          <img className="nav-logo" src="/logo.svg" alt="logo" />
+        </div>
+        <div id="nav-bar-subdiv-profpic" className="nav-bar-subdiv">
+          <button className='nav-bar-profpic'><img className="nav-bar-huey" src="/demoprofpic.png" alt="prof" /></button>
+        </div>
+      </div>
     </>
   );
 }
