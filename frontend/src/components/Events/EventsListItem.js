e2e7231850e39ae5a7a3615e96aea5f80f1bb093
Ryan Mullen <ryanfmullen@gmail.com>
2022-09-25 13:50:21 -0700

    add user attending to event index

--- frontend/src/components/Events/EventsListItem.js
+++ frontend/src/components/Events/EventsListItem.js
@@ -1,15 +1,23 @@
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom'
 import blankProfilePic from '../../blank-profile-picture.svg'
+import { BsCheckCircleFill } from 'react-icons/bs'
 import './EventsListItem.css'
 
 const EventsListItem = ({event}) => {
     const history = useHistory();
+    const sessionUser = useSelector(state => state.session.user);
+    // const attenders = Object.values(event.attendees)
+    // Object.values(event.attendees).map(person => {
+    //     console.log(person._id === sessionUser._id, "ATTEDNING THSI EFNTON")
+    // })
+
     const handleClick = (e) => {
         history.push(`/events/${event._id}`)
     }
-    console.log(new Date().toLocaleDateString())
+    // console.log(new Date().toLocaleDateString())
     let eventTime = new Date(event.eventTime)
-    console.log(eventTime.toLocaleDateString(), "EVENT")
+    // console.log(eventTime.toLocaleDateString(), "EVENT")
     if (!event || !event.attendees) return null;
     return(
         <>
@@ -21,11 +29,18 @@ const EventsListItem = ({event}) => {
                     <ul>
                         <li className='event-list-item-title'>{event.title}</li>
                         <li className='event-list-item-address'><span>WHERE: </span>{event.address}</li>
-                        {/* {new Date().toLocaleDateString() > eventTime.toLocaleDateString() ? <li className='event-list-item-when-expired'><span>WHEN: </span>Expired</li> :  */}
-                        <li className='event-list-item-when'><span>WHEN: </span>{`${(eventTime.getUTCHours() % 12) === 0 ? 12 : eventTime.getUTCHours() % 12}:${('0' + eventTime.getUTCMinutes()).slice(-2) } ${eventTime.getUTCHours() >=12 ? "PM" : "AM"}`}</li>
-                            {/* } */}
+                        { new Date().toLocaleDateString().slice(2,4) - eventTime.toLocaleDateString().slice(2,4) === 0 ? 
+                            <li className='event-list-item-when'><span>WHEN: </span>Today at {`${(eventTime.getUTCHours() % 12) === 0 ? 12 : eventTime.getUTCHours() % 12}:${('0' + eventTime.getUTCMinutes()).slice(-2) } ${eventTime.getUTCHours() >=12 ? "PM" : "AM"}`}</li>
+                        :  
+                            <li className='event-list-item-when'><span>WHEN: </span>Tomorrow at {`${(eventTime.getUTCHours() % 12) === 0 ? 12 : eventTime.getUTCHours() % 12}:${('0' + eventTime.getUTCMinutes()).slice(-2) } ${eventTime.getUTCHours() >=12 ? "PM" : "AM"}`}</li>
+                        }
                         <li className='event-list-item-description'><span>WHAT: </span>{event.description}</li>
-                        <li>{Object.values(event.attendees).length} attending </li>
+                        <div className='event-list-item-attendees-container'>
+                            <li className='event-list-item-attendees'>{Object.values(event.attendees).length} attending </li>
+                            {Object.values(event.attendees).map(person => {
+                                if (person._id === sessionUser._id) return <div className='attending-icon-container'><p className='event-list-item-you-attending'>Attending</p> <BsCheckCircleFill className='attending-icon'/></div>
+                            })}
+                        </div>
                     </ul>
                 </div>
             </div>
